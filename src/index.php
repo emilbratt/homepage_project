af96419f2f24d6e994eeb3dafa39556cfbc1ed2d
emil bratt <emilbratt@gmail.com>
2021-05-13 16:00:55 +0100

    working on frontpage layout

--- src/index.php
+++ src/index.php
@@ -8,33 +8,37 @@ Header::show(basename(htmlentities($_SERVER['PHP_SELF'])));
 
 <div class="greybox">
 
-    <div class="greyboxbody">
+    <div class="greybox_title_center">
     <h3 style="text-align: center;">John Travolta</h3>
     </div>
 
 
 
     <div class="greybox_inline_block" >
+
         <div class="greyboxbody" id="left_side">
         <h3>Programming</h3>
         <p>Check out my projects on this site or on github</p>
         </div>
-    <!-- </div>
 
-    <div class="greybox"> -->
         <div class="greyboxbody" id="left_side">
         <h3>Photographing</h3>
         <p>Nature, Weddings, People and more</p>
         </div>
+
         <div class="greyboxbody" id="left_side">
         <h3>Undergrad student</h3>
         <p>I have a special love for computers</p>
         </div>
+
     </div>
 
     <div class="greybox_inline_block" >
         <div class="greyboxbody" id="right_side" >
-        <img src="/extra/profile_pic.png" alt="" class="profile_pic" style="">
+        <img src="/extra/profile_pic.jpg" alt="" class="profile_pic" style="">
+        <figcaption style="text-align: left; margin-left: 3%; margin-right: 3%;">
+            Image Caption textImage Caption textImage Caption textImage Caption text
+        </figcaption>
         </div>
     </div>
 </div>
